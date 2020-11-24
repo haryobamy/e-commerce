f4c1c6d0442be71b7d83f8dcc8eb449eb4e0de0d
haryobamy <haryobamy.badmus@gmail.com>
2020-11-24 10:31:55 +0100

    implementing the navbar and links

---
+++ src/components/Default.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+ class Default extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default Default;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+import { ButtonContainer } from './Button'
+
 
  class Navbar extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <NavWrapper className='navbar navbar-expand-sm navbar-dark px-sm-5'>
+                <Link to='/'>
+                    <img src={require("../img/logo.jpg")}
+                     alt='store'
+                    className='navbar-brand' />
+                </Link>
+                <ul className='navbar-nav align-item-center'>
+                    <li className='nav-item ml-5'>
+                        <Link to='/' className='nav-link'>
+                            products
+                        </Link>
+                    </li>
+                </ul>
+                <Link to='/cart' className='ml-auto'>
+                    <ButtonContainer>
+                       <span classname='mr-2'> <i className='fas fa-cart-plus'  /></span>
+                       my cart
+                    </ButtonContainer>
+                </Link>
+            </NavWrapper>
         )
     }
 }
-export default Navbar;
+
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link: (--mainWhite)!important;
+font-size:1.3rem;
+text-transform:capitalize;
+`
+
+
+
+export default Navbar;  
